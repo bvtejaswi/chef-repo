64064371fa164c723189ec794a4981a603c2ab48
bvtejaswi <bondila.venkatatejaswi@progress.com>
2023-03-29 10:21:15 +0000

    first commit

---
+++ cookbooks/apache/recipes/server.rb
@@ -0,0 +1,13 @@
+package 'httpd'
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2023, The Authors, All Rights Reserved.
